2d2920040fc2ef807d3b0c903e94fc574c58e089
Vinnie117 <hhang@hotmail.de>
2021-10-31 17:41:28 +0100

    Change location of master file

--- Code/Master.R
+++ Code/Master.R
@@ -3,18 +3,17 @@
 # to generate all monthly charts automatically
 
 # load data
-source("Data Input.R")
+source("Code/Data Input.R")
 
 
 # generate heatmap of monthly returns
-source("Analysis_heatmap.R")
+source("Code/Analysis_heatmap.R")
 plot_heatmap
 
 # generate stock to flow
-source("Analysis_stock_to_flow.R")
+source("Code/Analysis_stock_to_flow.R")
 plot_price_halvings
 plot_s2f
-
 
 
 
