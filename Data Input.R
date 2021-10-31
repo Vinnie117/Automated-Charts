7473cfaf6e1eeb08431c85dce3914102690e3917
Vinnie117 <hhang@hotmail.de>
2021-10-31 16:43:20 +0100

    Start of data input

---
+++ Data Input.R
@@ -0,0 +1,56 @@
+########## Data Input ##########
+
+# Libraries
+library(crypto2)
+library(tidyverse)
+
+#### Functions
+
+# waiting between two API calls
+
+wait <- function(seconds){
+  
+    for (i in 1:seconds){
+    text <- paste0("Waiting between API Calls: ", i, " seconds of ", seconds)
+    print(text)
+    Sys.sleep(1)
+  }
+}
+
+
+#### Loading data
+
+# all active coins
+coins <- crypto_list(only_active=TRUE)
+
+# historical Bitcoin prices
+btc_historic_raw <- crypto_history(coins, limit=1, start_date="20130430")
+
+
+
+#### Data Preparation
+
+# select relevant data
+df_btc <- btc_historic_raw[,c(1,10)]
+
+# only keep the day date without time
+df_btc$timestamp <- substr(df_btc$timestamp,1,10) %>% as.Date()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
